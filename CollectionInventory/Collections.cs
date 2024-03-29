024dc2beefb7a384abbebf45afeaa5ceb5c9200d
lmarquardt7 <lmarquardt7@student.cvtc.edu>
2024-03-29 14:19:37 -0500

    worked on classes but they need work yet

--- CollectionInventory/Collections.cs
+++ CollectionInventory/Collections.cs
@@ -2,6 +2,11 @@
 //Date: 03/29/2024
 //Purpose: Manage the collections that belong to owners and are made of items
 
+
+/////////// ****************** NEEDs WORK *********************************
+///
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +17,66 @@ namespace CollectionInventory
 {
     internal class Collections
     {
+        private string mFirstName;
+        private string mLastName;
+
+        private string mCollectionName;
+        private string mCollectionDescription;
+        private List<string> mItemsList;
+        //private List<string> mItemsList = new List<Items>();
+
+        public string FirstName 
+            { get { return mFirstName; } set {  mFirstName = value; } }
+
+        public string LastName 
+            {  get { return mLastName; } set {  mLastName = value; } }
+
+        public string CollectionName 
+            { get { return mCollectionName; } set { mCollectionName = value; } }
+
+        public string CollectionDescription 
+            {  get { return mCollectionDescription; } set { mCollectionDescription = value; } }
+
+        public List<string> ItemsList 
+            { get { return mItemsList; } set { mItemsList = value; } }
+
+        ////Adds items to the collection
+        //public void AddItem(string item)
+        //{
+        //    Items.Add(item);
+        //}
+
+        // Default Constructor
+        public Collections()
+        {
+            mFirstName = string.Empty;
+            mLastName = string.Empty;
+            mCollectionName = string.Empty;
+            mCollectionDescription = string.Empty;
+            mItemsList = new List<string>();
+            //for (int count = 0; count  < mItemsList.Count; count++)
+            //{
+            //    mItemsList.Add(string.Empty);
+            //}
+        }
+
+        //Overload Constructor
+        public Collections(string FirstName)
+        {
+            mFirstName = FirstName;
+            mLastName = string.Empty;
+            mCollectionName = string.Empty;
+            mCollectionDescription = string.Empty;
+            mItemsList = new List<string>();
+            //for (int count = 0; count  < mItemsList.Count; count++)
+            //{
+            //    mItemsList.Add(string.Empty);
+            //}
+        }
+
+        public string DisplayCollection()
+        {
+            return mFirstName + " " + mLastName + "'s " + mCollectionName;
+        }
     }
 }
--- CollectionInventory/Items.cs
+++ CollectionInventory/Items.cs
@@ -12,5 +12,64 @@ namespace CollectionInventory
 {
     internal class Items
     {
+
+        //Define Class or Module Level Variables
+        private string mItemName;
+        private string mItemDescription;
+        private decimal mItemOriginalPrice;
+        private decimal mItemCurrentValue;
+        private decimal mItemProfit;
+
+        //Getters and Setters
+        public string ItemName
+        { get { return mItemName; } set { mItemName = value; } }
+
+        public string ItemDescription
+        { get { return mItemDescription; } set { mItemDescription = value; } }
+
+        public decimal ItemOriginalPrice
+        { get { return mItemOriginalPrice; } set { mItemOriginalPrice = value; } }
+
+        public decimal ItemCurrentValue
+        { get { return mItemCurrentValue; } set { mItemCurrentValue = value; } }
+
+        public decimal ItemProfit
+        { get { return mItemProfit; } set { mItemProfit = value; } }
+
+
+        //Default Constructor
+
+        public Items()
+        {
+            mItemName = string.Empty;
+            mItemDescription = string.Empty;
+            mItemOriginalPrice = 0;
+            mItemCurrentValue = 0;
+            mItemProfit = 0;
+        }
+
+        //Overload Constructor
+        public Items(string ItemName)
+        {
+            mItemName = ItemName;
+            mItemDescription = string.Empty;
+            mItemOriginalPrice = 0;
+            mItemCurrentValue = 0;
+            mItemProfit = 0;
+        }
+
+        public string DisplayItem()
+        {
+            // This will provide my formatted data that will be displayed in the listbox
+
+            if (mItemProfit < 0)
+            {
+                return mItemName + " (Loss of $" + mItemProfit.ToString() + ")";
+            }
+            else
+            {
+                return mItemName + " (Profit of $" + mItemProfit.ToString() + ")";
+            }
+        }
     }
 }
